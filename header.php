c4ce86a5bf6733b459cf3c6ff58e7f8de689b1fb
Linus Groschke <contact@elektronisch.dev>
2023-04-21 19:42:08 +0200

    Improve description

--- header.php
+++ header.php
@@ -2,21 +2,18 @@
 <html lang="<?= $language ?>">
 <head>
     <meta charset="utf-8">
-    <title>Linus – elektronisch.dev</title>
+    <title>Linus Groschke – elektronisch.dev</title>
     <meta name="viewport" content="initial-scale=1"/>
-    <meta name="description" content="Yet another computer enthusiast"/>
+    <meta name="description"
+          content="Linus Groschke, *2003, lebt in Augsburg. Dort besucht er aktuell die dreizehnte Klasse der Freien Waldorfschule Augsburg. Er engagiert sich als Hobby-Programmierer. Bei der SSV Augsburg, bei Fridays For Future und im Klimacamp Augsburg ist er politisch aktiv."/>
     <meta name="referrer" content="no-referrer">
 
     <meta property="og:url" content="https://elektronisch.dev/"/>
-    <meta property="og:title" content="Linus – elektronisch.dev"/>
-    <meta property="og:description" content="Yet another computer enthusiast"/>
+    <meta property="og:title" content="Linus Groschke – hobby programmer – elektronisch.dev"/>
+    <meta property="og:description" content="Linus Groschke, *2003, lebt in Augsburg."/>
     <meta property="og:image" content="https://elektronisch.dev/assets/img/logo.png"/>
     <meta property="og:type" content="website"/>
     <meta property="og:locale" content="<?= $language ?>"/>
-
-    <meta property="twitter:card" content="summary"/>
-    <meta property="twitter:site" content="@elektr0nisch"/>
-    <meta property="twitter:creator" content="@elektr0nisch"/>
 
     <link rel="shortcut icon" type="image/webp" href="/assets/img/logo.webp"/>
     <link rel="stylesheet" href="/assets/css/style.css">
