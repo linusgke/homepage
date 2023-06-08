868e4e94c14f5ca0706fa4694b3b6149c6925273
Linus Groschke <contact@elektronisch.dev>
2023-06-09 00:43:45 +0200

    Add AbuseIPDB

--- index_de.php
+++ index_de.php
@@ -118,4 +118,8 @@
     <a class="badge" href="https://www.wikidata.org/wiki/Special:Contributions/Linus_Groschke">
         <img src="/assets/img/shields/wikidata.svg" alt="Wikidata">
     </a>
+    <br>
+    <a class="badge" href="https://www.abuseipdb.com/user/116584" title="AbuseIPDB is an IP address blacklist for webmasters and sysadmins to report IP addresses engaging in abusive behavior on their networks">
+        <img src="https://www.abuseipdb.com/contributor/116584.svg" alt="AbuseIPDB Contributor Badge" style="width: 176px;border-radius: 5px;border-top: 5px solid #058403;border-right: 5px solid #111;border-bottom: 5px solid #111;border-left: 5px solid #058403;padding: 5px;background: #35c246 linear-gradient(rgba(255,255,255,0), rgba(255,255,255,.3) 50%, rgba(0,0,0,.2) 51%, rgba(0,0,0,0));padding: 5px;">
+    </a>
 </div>
